226ea89624897a0f268ab518cf387b2be2760ae6
sv3nn <sv3nn@users.noreply.github.com>
2018-05-18 16:43:02 +0200

    Feedback Ivon

--- main.js
+++ main.js
@@ -13,12 +13,4 @@ $(function() {
 		$(this).parent().css('padding', '0');
         $(this).parent().html('');
     });
-	
-	$('.btn-info').click(function() {
-		$('#info').html($(this).next().html());
-	});
-	
-	$('#info').on('click', '.close', function (){
-        $(this).parent().html('');
-    });
 });
