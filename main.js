a765819ad3869518963ac32959daa89702a89241
sv3nn <sv3nn@users.noreply.github.com>
2018-05-22 15:05:20 +0200

    Iframe issue test 2

--- main.js
+++ main.js
@@ -13,21 +13,4 @@ $(function() {
 		$(this).parent().css('padding', '0');
         $(this).parent().html('');
     });
-	
-	 adjustIframeHeight('#chart-iframe', 1400, 20);
 });
-
-function adjustIframeHeight(iframe, minHeight, fix){
-
-    var height = 0, $frame = $(iframe);
-    if(typeof fix=='undefined') fix = 0;
-
-    setInterval(function(){
-        if( typeof $frame.contents()!=null && typeof $frame.contents() !='undefined' && $frame.contents() !=null && $frame.attr('src')!='') {
-            curHeight = $frame.contents().find('body').height();
-            $frame.css('height', height + fix); // you might need to add some extra values like +20px.
-        } else {
-            $frame.css('height', minHeight); // minimum height for the iframe.
-        }
-    },200);
-}
